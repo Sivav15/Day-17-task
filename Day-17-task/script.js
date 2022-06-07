71e30baed428ec629aa832534a627c093fc03658
Sivanathan <104622092+Sivav15@users.noreply.github.com>
2022-06-07 13:35:32 +0530

    Add files via upload

---
+++ Day-17-task/script.js
@@ -0,0 +1,213 @@
+// function happy_independence_day(a, b) {
+//     console.log(a);
+//     b();
+// }
+
+// function a3(a, b) {
+//     console.log(a);
+
+//     b();
+// }
+
+// function a5(a, b) {
+//     console.log(a);
+//     b();
+// }
+
+// function a7(a, b) {
+//     console.log(a);
+//     b();
+// }
+
+// function a9(a, b) {
+//     console.log(a);
+//     b();
+// }
+
+
+// happy_independence_day(10, function () {
+//     console.log(9);
+
+//     a3(8, function () {
+//         console.log(7);
+
+//         a5(6, function () {
+//             console.log(5);
+
+//             a7(4, function () {
+//                 console.log(3);
+
+//                 a9(2, function a() {
+//                     console.log(1);
+//                     
+//                 });
+
+//             });
+//         });
+
+
+//     });
+
+// });
+
+
+
+
+
+
+
+
+
+setTimeout(function () {
+    document.getElementById('text').innerHTML = "10";
+
+    setTimeout(function () {
+        document.getElementById('text').innerHTML = "9";
+
+        setTimeout(function () {
+            document.getElementById('text').innerHTML = "8";
+
+            setTimeout(function () {
+                document.getElementById('text').innerHTML = "7";
+
+                setTimeout(function () {
+                    document.getElementById('text').innerHTML = "6";
+
+                    setTimeout(function () {
+                        document.getElementById('text').innerHTML = "5";
+                        setTimeout(function () {
+                            document.getElementById('text').innerHTML = "4";
+                            setTimeout(function () {
+                                document.getElementById('text').innerHTML = "3";
+                                setTimeout(function () {
+                                    document.getElementById('text').innerHTML = "2";
+                                    setTimeout(function () {
+                                        document.getElementById('text').innerHTML = "1";
+                                        setTimeout(function () {
+                                            document.getElementById('text').innerHTML = "Happy Independence Day";
+
+                                        }, 1000)
+                                    }, 1200)
+                                }, 1200)
+                            }, 1200)
+                        }, 1200)
+                    }, 1200)
+                }, 1200)
+            }, 1200)
+        }, 1200)
+    }, 1200)
+}, 1200)
+
+
+// class person {
+//     constructor();
+// }
+
+
+// let obj = new person();
+
+
+
+
+// let obj2 = new person();
+// let obj3 = new person();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// setTimeout(()=>{
+//     console.log(10);
+// },()=>{
+//     console.log(9);
+// },1000);
+
+// setTimeout(()=>{
+//     console.log(9);
+// },2000);
+
+// setTimeout(()=>{
+//     console.log(8);
+// },3000);
+
+// setTimeout(()=>{
+//     console.log(7);
+// },4000);
+
+// setTimeout(()=>{
+//     console.log(6);
+// },5000);
+// setTimeout(()=>{
+//     console.log(5);
+// },6000);
+// setTimeout(()=>{
+//     console.log(4);
+// },7000);
+// setTimeout(()=>{
+//     console.log(3);
+// },8000);
+// setTimeout(()=>{
+//     console.log(2);
+// },9000);
+// setTimeout(()=>{
+//     console.log(1);
+// },10000);
+// setTimeout(()=>{
+//     console.log("Happy Independence Day");
+// },11000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.console.log("siva")
+
+
+
+
+
+
+
+
+
+
+
+
+
